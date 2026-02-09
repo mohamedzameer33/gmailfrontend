77850d9c809b4ffa71ecb13ecb619d9e5845fdf6
mohamedzameer33 <mohamedzameermpm123@gmail.com>
2026-02-09 20:47:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
-import EmailHome from './assets/EmailHome';
-import NewTemplate from './assets/NewTemplate';
+import EmailHome from './EmailHome';
+import NewTemplate from './NewTemplate';
 
 
 /** * PROFESSIONAL LOGIN COMPONENT
